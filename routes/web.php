4cfa0e80170c1e369d0248db4cafba34dc4cd6fe
ILJAZI Dardan <Dardan.ILJAZI@cpnv.ch>
2018-09-25 10:36:24 +0200

    Testing some things

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Symfony\Component\Finder\Finder;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+
+
+    $finder = new Finder();
+    //print_r(__DIR__);
+    $finder->files()->in("N:\\")->name("(/\.php|/\.md|/\.txt|/\.pdf|/\.docx|/\.vsdx|/\.css|/\.html|/\.doc|/\.rtf|/\.js|/\.xml|/\.json|/\.log|/\.ipt|/\.odt|/\.wks|/\.wpd)$/");
+
+    foreach ($finder as $file) {
+        // dumps the absolute path
+        echo $file->getFilename() . "\r\n";
+//        var_dump($file->getRealPath());
+//
+//        // dumps the relative path to the file, omitting the filename
+//        var_dump($file->getRelativePath());
+//
+//        // dumps the relative path to the file
+//        var_dump($file->getRelativePathname());
+    }
 });
