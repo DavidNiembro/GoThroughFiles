cedf0d34d415751a2adb177e4c53f5bccd121d22
NIEMBRO David <David.NIEMBRO@cpnv.ch>
2018-12-06 12:13:45 +0100

    fix function date and size

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -24,12 +24,9 @@ class Modal extends Component {
 
     render() {
         let file = this.props.data
-        /* Fonction format date */
-       const time = getDate()
-        let taille = fileSizeSI(1355785)
 
-       const containerClass = this.props.modalOpened ? 'modalContainer modalContainerActive' : 'modalContainer';
-       return (
+        const containerClass = this.props.modalOpened ? 'modalContainer modalContainerActive' : 'modalContainer';
+        return (
             <div className={containerClass}>
                 <div className='modalHeader'>
                     <h1 style={{paddingTop:20}}>{this.props.data && this.props.data.Name}</h1>
@@ -37,10 +34,10 @@ class Modal extends Component {
                 </div>
                 <div className='modalBody'>
                     <ul>
-                        <li><strong>Taille : </strong> {taille}</li>
+                        <li><strong>Taille : </strong> {fileSizeSI(file && file.meta && file.meta.size)}</li>
                         <li><strong>Emplacement: </strong>{file && file.Path}</li>
-                        <li><strong>Date de modification : </strong>{file && file.meta && file.meta.mtime}</li>
-                        <li><strong>Date de création : </strong>{file && file.meta && file.meta.birthtime}</li>
+                        <li><strong>Date de modification : </strong>{getDate(file && file.meta && file.meta.mtime)}</li>
+                        <li><strong>Date de création : </strong>{getDate(file && file.meta && file.meta.birthtime)}</li>
                     </ul>
                 </div>
                 <div className="buttonModalOpenFile" onClick={(e)=>this.openfile(e,file.Path)}   > ouvrir le fichier</div>
