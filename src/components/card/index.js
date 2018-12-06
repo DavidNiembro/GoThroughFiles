d11215d5ad9ac4c2cef9a07e6fbc90b3c1e66c02
NIEMBRO David <David.NIEMBRO@cpnv.ch>
2018-12-06 10:33:30 +0100

    refactoring css

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -19,25 +19,22 @@ class Card extends Component {
         let total = name.length;
         let nameFinal = name.substr(0,index);
         let extention = name.substr(index+1,total-index);
+        
         return (
+            <div className="Card" onClick={()=>this.props.modal()}>
 
+                <h3 style={{color:"white"}}>{nameFinal}</h3>
 
-        <div className="Card" key="key1" onClick={()=>this.props.modal()}>
-
-            <h3 style={{color:"white"}}>{nameFinal}</h3>
-
-            <div className="trait"></div>
+                <div className="trait"></div>
             
+                <div onClick={(e)=>this.openfile(e,file.Path)}  className="openFileButton">
+                    <span className="openFileText">{"ouvrir"}</span>
+                </div>
 
-
-
-            <div onClick={(e)=>this.openfile(e,file.Path)}  className="openFileButton">
-                <span className="openFileText">{"ouvrir"}</span>
-            </div>
                 <div className="extension">
-                {extention}
+                    {extention}
+                </div>
             </div>
-        </div>
         );
     }
 }
