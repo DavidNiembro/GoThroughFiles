e11ec4d2939e1e79f8948b1bfe3aa886fba9ece4
NIEMBRO David <David.NIEMBRO@cpnv.ch>
2018-12-13 12:59:16 +0100

    update main

--- main.js
+++ main.js
@@ -16,16 +16,22 @@ const startUrl = process.env.ELECTRON_START_URL || url.format({
 
 app.on("ready", function() {
     
-    mainWindow = new BrowserWindow({ width: 900, height: 600, minHeight: 800, minWidth: 900,frame: false });
-    /*
-        const startUrl =
-        process.env.ELECTRON_START_URL ||
-        url.format({
-            pathname: path.join(__dirname, "/../build/index.html"),
-            protocol: "file:",
-            slashes: true
-        });
-    */
+   // Config of the application window for Windows
+    mainWindow = new BrowserWindow({ width: 900, height: 600, minHeight: 800, minWidth: 900,frame: false, backgroundColor: '#070b0e',show: false, center: true,icon: path.join(__dirname, "src/assets/logo/gothroughfiles.ico"),  fullscreenWindowTitle: true, defaultFontFamily: "fantasy", title: "GoThroughFiles", webPreferences: {
+        webSecurity: false
+    }, node: {
+        __dirname: false,
+        __filename: false
+    } });
+    // For MacOS
+    if (process.platform === 'darwin') {
+    mainWindow = new BrowserWindow({ width: 900, height: 600, minHeight: 800, minWidth: 900,frame: false, title:"GoThroughFiles", icon: path.join(__dirname, "src/assets/logo/gothroughfiles.icns"), backgroundColor: "#070b0e", type: "textured" });
+    }
+    // For Linux
+    if (process.platform === 'linux') {
+    mainWindow = new BrowserWindow({ width: 900, height: 600, minHeight: 800, minWidth: 900,frame: false, title:"GoThroughFiles", icon: path.join(__dirname, "src/assets/logo/gothroughfiles64x64.png"), backgroundColor: "#070b0e", type: "textured" });
+    }
+
     setTimeout(mainWindows, 300);
 
     function mainWindows() {
